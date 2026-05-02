6e02a71b09657b0faf1cdceddf43674d8b7da7c7
Artem-WebDeveloper <ave_art@icloud.com>
2026-05-02 19:02:22 +0300

    feat: add ui and add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const StyledApp = styled.div`
+  background-color: aquamarine;
+  padding: 20px;
+`;
+
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Button onClick={() => alert('butt')}>DDDD</Button>
+
+        <Button>ewfe</Button>
+
+        <Input placeholder="ssss" type="number" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
