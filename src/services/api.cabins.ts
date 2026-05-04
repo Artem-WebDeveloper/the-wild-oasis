0c9c222e5e4165f7d7dfdfcc86b6b32ce7f762a4
Artem-WebDeveloper <ave_art@icloud.com>
2026-05-04 16:42:40 +0300

    feat: add supabase

---
+++ src/services/api.cabins.ts
@@ -0,0 +1,13 @@
+import type { Cabin } from '../types';
+import supabase from './supabase';
+
+export async function getCabins(): Promise<Cabin[]> {
+  const { data, error } = await supabase.from('cabins').select('*');
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be loaded');
+  }
+
+  return data;
+}
